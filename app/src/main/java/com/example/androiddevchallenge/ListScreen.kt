427eea51104ba8e188fafa45fd2bcd7ae2535e73
Matthias Kellnhofer <matthias@kellnhofer.com>
2021-02-26 00:48:12 +0100

    Add list screen

---
+++ app/src/main/java/com/example/androiddevchallenge/ListScreen.kt
@@ -0,0 +1,131 @@
+package com.example.androiddevchallenge
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.androiddevchallenge.data.DogRepo
+import com.example.androiddevchallenge.data.dummyDogs
+import com.example.androiddevchallenge.model.Dog
+
+@Composable
+fun ListScreen(
+    repository: DogRepo,
+    navigateToDogAction: (Long) -> Unit
+) {
+    val dogs by repository.getAllDogs().observeAsState(emptyList())
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name)) }
+            )
+        }
+    ) {
+        DogList(
+            modifier = Modifier.fillMaxWidth(),
+            dogs = dogs,
+            onClick = { navigateToDogAction(it.id) },
+        )
+    }
+}
+
+@Composable
+fun DogList(
+    modifier: Modifier,
+    dogs: List<Dog>,
+    onClick: (Dog) -> Unit,
+) {
+    val scrollState = rememberLazyListState()
+
+    LazyColumn(
+        state = scrollState,
+        modifier = Modifier
+            .background(color = MaterialTheme.colors.background)
+    ) {
+        items(dogs) {
+            DogItem(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                dog = it,
+                onClick = { onClick(it) },
+            )
+            Divider(startIndent = 98.dp)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DogListPreview() {
+    DogList(Modifier, dummyDogs) {}
+}
+
+@Composable
+fun DogItem(
+    modifier: Modifier,
+    dog: Dog,
+    onClick: () -> Unit,
+) {
+    Row (
+        modifier = modifier
+            .background(color = MaterialTheme.colors.background)
+            .clickable(onClick = onClick)
+            .padding(horizontal = 24.dp, vertical = 12.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .size(50.dp),
+            shape = CircleShape,
+        ) {
+            Image(
+                painter = painterResource(dog.imageResId),
+                contentDescription = null,
+            )
+        }
+        Column(
+            modifier = Modifier
+                .padding(start = 24.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(
+                text = dog.name,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            CompositionLocalProvider(
+                LocalContentAlpha provides ContentAlpha.medium
+            ) {
+                Text(
+                    text = dog.bred,
+                    style = MaterialTheme.typography.body2,
+                    fontSize = 12.sp
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DogItemPreview() {
+    DogItem(Modifier, dummyDogs[0]) {}
+}
--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -18,16 +18,15 @@ package com.example.androiddevchallenge
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.androiddevchallenge.data.DogRepo
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             MyTheme {
                 MyApp()
@@ -36,12 +35,11 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-// Start building your app here!
+val repository = DogRepo()
+
 @Composable
 fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Text(text = "Ready... Set... GO!")
-    }
+    ListScreen(repository = repository, navigateToDogAction = {})
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
